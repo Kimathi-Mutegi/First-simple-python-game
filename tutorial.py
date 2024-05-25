e7f2abd94bc17eaa7e658cb3990b9cd78140bdbe
Kimathi-Mutegi <kimathimutegi186@gmail.com>
2024-05-25 07:11:23 -0700

    first commit

--- tutorial.py
+++ tutorial.py
@@ -11,7 +11,7 @@ test_font = pygame.font.Font('graphics/font/Pixeltype.ttf', 50)
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
-score_surf = test_font.render("RUNNER", False, "Black")
+score_surf = test_font.render("RUNNER", True, (64, 64, 64))
 score_rect = score_surf.get_rect(center= (400, 50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
@@ -29,14 +29,29 @@ while True:
         #    if player_rect.collidepoint(event.pos): 
         #        print('collision')
 
+        if event.type == pygame.KEYUP:
+            print('key up')
+
+        if event.type == pygame.KEYDOWN:
+            print('key down')
+
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0, 300))
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+    #pygame.draw.line(screen, 'Gold', (0,0), pygame.mouse.get_pos(),10)
+    #pygame.draw.ellipse(screen, 'Brown', pygame.Rect(50, 200, 100, 100))
     screen.blit(score_surf, score_rect)
+
     snail_rect.x-= 4
     if snail_rect.x== -100:
         snail_rect.x = 800
     screen.blit(snail_surf, snail_rect)
     screen.blit(player_surf, player_rect)
+
+    #keys = pygame.key.get_pressed()
+    #if keys[pygame.K_a]:
+    #    print('Australia')
 
     #if snail_rect.colliderect(player_rect):
     #    print('collision')
